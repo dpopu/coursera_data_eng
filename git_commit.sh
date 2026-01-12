700f88eb83ed1f2eca1d7972bd40b3bf833a2823
Daniel POPU <dpopu@yahoo.com>
2026-01-12 13:43:50 +0000

    add git_commit.sh file for future use.

--- git_commit.sh
+++ git_commit.sh
@@ -1,4 +1,4 @@
 git config --global user.email "dpopu@yahoo.com"
 git config --global user.name "Daniel POPU"
 
-git commit -m "add make file"
+git commit -m "add git_commit.sh file for future use."
